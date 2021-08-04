ed50a87e95e7d878474f859c4aa6c36d2f4e3029
AJITH KUMAR A <12ajith1998@gmail.com>
2021-08-04 14:22:36 +0530

    Add files via upload

---
+++ src/com/krish/Login.java
@@ -0,0 +1,33 @@
+package com.krish;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.krish.dao.LoginDao;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		String uname=request.getParameter("uname");
+		String pass=request.getParameter("pass");
+		
+		LoginDao da=new LoginDao();
+		if(da.check(uname, pass))
+		{
+			HttpSession session=request.getSession();
+			session.setAttribute("username", uname);
+			response.sendRedirect("welcome.jsp");
+		}
+		else
+		{
+			response.sendRedirect("login.jsp");
+		}
+	}
+}
